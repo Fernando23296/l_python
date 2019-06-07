3ad55aa86c11bcbd8acd6293453617b6a7fa9284
Bruno <fernando23296@gmail.com>
2019-06-06 20:27:25 -0400

    Commi

--- tp_uno.py
+++ tp_uno.py
@@ -27,3 +27,32 @@ def find_center(rect):
 
 center = find_center(box)
 print_point(center)
+print("-"*30)
+p1 = Point()
+p1.x = 3.0
+p1.y = 4.0
+p2 = p1
+
+
+
+import copy
+p3 = copy.copy(p1)
+p1.x += 9.0
+print(p2.x)
+print(p3.x)
+
+box2 = copy.copy(box)
+#el copy ayuda pero no al nivel embebido (a un nivel mas profundo no)
+print(box2.corner==box.corner)
+#la solucion es usar deepcopy
+box3 = copy.deepcopy(box)
+print(box3 is box)
+print(box3.corner is box.corner)
+
+'''si no estas seguro de si x atributo es parte del objeto
+se usa hasattr
+'''
+p = Point()
+print(hasattr(p,'x'))
+print(hasattr(p,'z'))
+
